a24970b5a67a1804c2be0e7b09a8f1059003825a
vladimir <vladimirmawla2@gmail.com>
2022-05-23 10:42:14 +0300

    Adding second dice on refresh

--- scripts/dice.js
+++ scripts/dice.js
@@ -17,6 +17,20 @@ window.onload = (event) => {
     } if(x == 6){
         img1.src = "./images/dice6.png";
     }
-
-
+    let y = Math.floor(Math.random() * 6) +1;
+    let img2 = document.getElementById("img2")
+    if(y == 1){
+        img2.src = "./images/dice1.png";
+    } if(y == 2){
+        img2.src = "./images/dice2.png";
+    } if(y == 3){
+        img2.src = "./images/dice3.png";
+    } if(y == 4){
+        img2.src = "./images/dice4.png";
+    } if(y == 5){
+        img2.src = "./images/dice5.png";
+    } if(y == 6){
+        img2.src = "./images/dice6.png";
+    }
+    
 }
