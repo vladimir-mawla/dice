d724334d720aef3155a95916cbb8adf2772f3abb
vladimir <vladimirmawla2@gmail.com>
2022-05-24 13:55:48 +0300

    Editing save score functions

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,8 +1,9 @@
 window.onload = (event) => {
-  getScores();
   throw_dice();
 };
 var scores = { player1: 0, player2: 0 };
+var scores = JSON.parse(localStorage.getItem('score'))
+
 
 function throw_dice() {
   let x = Math.floor(Math.random() * 6) + 1;
@@ -14,21 +15,22 @@ function throw_dice() {
 
   if (x > y) {
     document.getElementById("title").innerHTML = "PLAYER 1 WINS";
-    scores.player1 += 1;
+    scores.player1 = scores.player1 + 1;
   } else if (x == y) {
     document.getElementById("title").innerHTML = "DRAW";
   } else {
     document.getElementById("title").innerHTML = "PLAYER 2 WINS";
-    scores.player2 += 1;
+    scores.player2 = scores.player2 + 1;
   }
+
   saveScores();
+  getScores();
 }
 
 function saveScores() {
   localStorage.setItem("score", JSON.stringify(scores));
 }
 function getScores() {
-  JSON.parse(
-    (document.getElementById("text1").innerHTML = localStorage.getItem("score"))
-  );
+    document.getElementById("text1").innerHTML = scores.player1;
+    document.getElementById("text2").innerHTML = scores.player2;
 }
